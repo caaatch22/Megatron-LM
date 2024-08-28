1b4c22a2714e66776537db305893ea4034349dfd
caaatch22 <mr.liumingjie@gmail.com>
2024-08-28 09:19:26 +0000

    chore: remove debug info

--- megatron/core/transformer/dot_product_attention.py
+++ megatron/core/transformer/dot_product_attention.py
@@ -128,8 +128,6 @@ class DotProductAttention(MegatronModule):
         # to extract the queries.
         query = query.reshape(output_size[2], output_size[0] * output_size[1], -1)
         # [sk, b, np, hn] -> [sk, b * np, hn]
-        print(f'key.shape = {key.shape}')
-        print(f'key = {key.stride()}')
         key = key.view(output_size[3], output_size[0] * output_size[1], -1)
 
         # preallocting input tensor: [b * np, sq, sk]
